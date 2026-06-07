3454d9316aa1765dfa5ebc9f31ef6c39c28fc33f
Nikhil Patwari <nikhilpatwari@hotmail.com>
2026-06-07 18:22:59 +0530

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+        int [] indegree = new int[numCourses];
+        for(int i = 0; i< numCourses; i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i = 0;i<prerequisites.length; i++){
+            indegree[prerequisites[i][1]]++;
+            adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i< numCourses; i++){
+            if(indegree[i] == 0){
+                q.offer(i);
+            }
+        }
+        int processed = 0;
+        while(!q.isEmpty()){
+            int n = q.poll();
+            processed++;
+            for(int i : adj.get(n)){
+                indegree[i]--;
+                if(indegree[i] == 0){
+                    q.add(i);
+                }
+            }
+        }
+        return processed == numCourses;
+        
+    }
+}
