e4bebf1ff02f0ead730d89689285b7e5914748af
Nikhil Patwari <nikhilpatwari@hotmail.com>
2026-08-11 11:25:23 +0530

    Add: baseball-game - submission-3

---
+++ Data Structures & Algorithms/baseball-game/submission-3.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        int result = 0;
+        Stack<Integer> score = new Stack<>();
+        for (String i : operations) {
+            switch (i) {
+                case "+":
+                    int num2 = score.pop();
+                    int num1 = score.peek();
+                    score.push(num2);
+                    score.push(num1 + num2);
+                    result += num1 + num2;
+                    break;
+                case "D":
+                    score.push(score.peek() * 2);
+                    result += score.peek();
+                    break;
+                case "C":
+                    result -= score.pop();
+                    break;
+                default:
+                    score.push(Integer.valueOf(i));
+                    result += score.peek();
+            }
+        }
+        return result;
+    }
+}
