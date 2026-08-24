5bb4ebac73ca9ab2cf18e75253442029e5009323
Nikhil Patwari <nikhilpatwari@hotmail.com>
2026-08-24 13:32:28 +0530

    Add: serialize-and-deserialize-binary-tree - submission-5

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-5.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Codec {
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        serialize(root, sb);
+        return sb.toString();
+    }
+
+    public void serialize(TreeNode root, StringBuilder sb) {
+        if (root == null) {
+            sb.append("N ");
+            return;
+        }
+        sb.append(root.val);
+        sb.append(" ");
+        serialize(root.left, sb);
+        serialize(root.right, sb);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] strings = data.split(" ");
+        int[] i = new int[] {0};
+        return constructTree(strings, i);
+    }
+    public TreeNode constructTree(String[] strings, int[] i) {
+        if (i[0] > strings.length) {
+            return null;
+        }
+        TreeNode n = convertToNode(strings[i[0]]);
+        if (n == null)
+            return null;
+        i[0]++;
+        n.left = constructTree(strings, i);
+        i[0]++;
+        n.right = constructTree(strings, i);
+        return n;
+    }
+    private TreeNode convertToNode(String val) {
+        if (val.equals("N")) {
+            return null;
+        } else {
+            return new TreeNode(Integer.valueOf(val));
+        }
+    }
+    private int findNextIndex(String s, int i) {
+        while (i < s.length() && s.charAt(i) != '#') {
+            i++;
+        }
+        return i;
+    }
+}
